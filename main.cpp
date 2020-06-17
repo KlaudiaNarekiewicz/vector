57b8374b73f0e44cdce3d76b5d12c0c508660669
KlaudiaNarekiewicz <62132418+KlaudiaNarekiewicz@users.noreply.github.com>
2020-06-17 19:57:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,51 +6,51 @@ using namespace std;
 
 struct klasy
 {
-	string klasa;       
+   string klasa;       
 };
 class szkola
 {
-	vector <struct klasy> spis;
-	public:
-	void wypisz();
-	void wprowadz();
-	void zapiszDoPliku(const char *);              
+   vector <struct klasy> spis;
+   public:
+   void wypisz();
+   void wprowadz();
+   void zapiszDoPliku(const char *);              
 };
 void szkola::wypisz()
 {
-	for (vector<struct klasy>::iterator it = spis.begin(); it != spis.end(); ++it)
-	{
-		cout << it->klasa << "\n";
-	}
+   for (vector<struct klasy>::iterator it = spis.begin(); it != spis.end(); ++it)
+      {
+       cout << it->klasa << "\n";
+      }
 }
 void szkola::wprowadz()
 {
-	int ilosc_klas;
-	struct klasy wprowadz1;
-	cout << "Ilosc klas: ";
-	cin  >> ilosc_klas;
-	for (int i=0; i<ilosc_klas; i++)
-	{
-		cout << "Podaj nazwe klasy: ";
-		cin >> wprowadz1.klasa;
-		spis.push_back(wprowadz1); 
-	}
+   int ilosc_klas;
+   struct klasy wprowadz1;
+   cout << "Ilosc klas: ";
+   cin  >> ilosc_klas;
+   for (int i=0; i<ilosc_klas; i++)
+   {
+      cout << "Podaj nazwe klasy: ";
+      cin >> wprowadz1.klasa;
+      spis.push_back(wprowadz1); 
+   }
 }
 void szkola::zapiszDoPliku(const char *nazwa)
 {
-	ofstream plik;
-	plik.open(nazwa);
-	for (vector<struct klasy>::iterator it = spis.begin(); it != spis.end(); ++it)
-	{
-		plik << it->klasa << "\n";
-	}
-	plik.close();  
+   ofstream plik;
+   plik.open(nazwa);
+   for (vector<struct klasy>::iterator it = spis.begin(); it != spis.end(); ++it)
+   {
+      plik << it->klasa << "\n";
+   }
+   plik.close();  
 }
 int main(int argc, char** argv) 
 {
-	szkola k;
-	k.wprowadz();
-	k.wypisz();
- 	k.zapiszDoPliku("1.txt");
+   szkola k;
+   k.wprowadz();
+   k.wypisz();
+   k.zapiszDoPliku("1.txt");
 return 0;
 }
